435fcb3d7ad0249aa3b5e36beb76011173b95db0
CamronG <git@siege.anonaddy.com>
2022-09-30 22:11:04 -0400

    Add ConfiguredWidget Tests

---
+++ configurator_flutter/test/configured_widget_test.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+
+import 'package:configurator_flutter/configurator_flutter.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+
+void main() {
+
+  group( 'ConfiguredWidget Tests', () {
+
+    testWidgets('Config.of Test', (tester) async {
+
+      await tester.pumpWidget(
+        Configurator(
+          config: Configuration(),
+          builder: ( context, _ ) => ConfiguredWidget(
+            builder: ( _ ) =>  Container() ,
+          ),
+        ),
+      );
+
+      expect(
+            () => Config.of(tester.element(find.byType(Container))),
+        isNotNull,
+        reason: 'Configuration should not be null.',
+      );
+    });
+
+    testWidgets('Add Scope Rebuilds Test', (tester) async {
+
+      Completer<Configuration> completer = Completer();
+
+      int hitCount = 0;
+
+      await tester.pumpWidget(
+        Configurator(
+          config: Configuration(),
+          builder: ( context, _ ) => ConfiguredWidget(
+            builder: ( config ) {
+              hitCount++;
+
+              if ( hitCount == 1 ) {
+                var s1 = ProxyScope(name: 'test');
+                config.pushScope( s1 );
+              } else if ( hitCount == 2 ) {
+                completer.complete( config );
+              } else {
+                throw Exception( 'Should not be hit a 3rd time.' );
+              }
+
+              return Container();
+            },
+          ),
+        ),
+      );
+
+      await tester.pumpAndSettle();
+
+      await expectLater( completer.future, completion( isNotNull ) ).timeout( const Duration( seconds: 1 ) );
+
+    });
+  });
+}
