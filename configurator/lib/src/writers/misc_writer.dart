4ecbcfef9aa75ea9f4a101f7024f7cac6ca95d97
cgiuliani <camrongiuliani@pm.me>
2023-09-19 20:51:52 -0400

    fix: cast misc types

--- configurator/lib/src/writers/misc_writer.dart
+++ configurator/lib/src/writers/misc_writer.dart
@@ -27,40 +27,43 @@ class MiscWriter extends Writer {
 
   List<Method> _getGetters() {
     return _settings.map((e) {
+
+      var type = () {
+        if (e.value is String) {
+          return 'String';
+        } else if (e.value is int) {
+          return 'int';
+        } else if (e.value is double) {
+          return 'double';
+        } else if (e.value is bool) {
+          return 'bool';
+        } else if (e.value is List) {
+          var types = (e.value as List).map((e) {
+            return e.runtimeType.toString();
+          }).toSet().toList();
+
+          if (types.length == 1) {
+            if (types.first.contains('_Map')) {
+              return 'List<Map<String, dynamic>>';
+            }
+
+            return 'List<${types.first}>';
+          }
+
+          return 'List<dynamic>';
+        }
+
+        return 'dynamic';
+      }();
+
       return Method( ( builder ) {
         builder
           ..name = e.name.canonicalize
           ..type = MethodType.getter
-          ..returns = refer(() {
-            if (e.value is String) {
-              return 'String';
-            } else if (e.value is int) {
-              return 'int';
-            } else if (e.value is double) {
-              return 'double';
-            } else if (e.value is bool) {
-              return 'bool';
-            } else if (e.value is List) {
-              var types = (e.value as List).map((e) {
-                return e.runtimeType.toString();
-              }).toSet().toList();
-
-              if (types.length == 1) {
-                if (types.first.contains('_Map')) {
-                  return 'List<Map<String, dynamic>>';
-                }
-
-                return 'List<${types.first}>';
-              }
-
-              return 'List<dynamic>';
-            }
-
-            return 'dynamic';
-          }())
+          ..returns = refer(type)
           ..lambda = true
           ..body = Code( () {
-            return '_config.misc("${e.name}")';
+            return '_config.misc("${e.name}")${type == 'dynamic' ? '' : ' as $type'}';
           }() );
       });
     }).toList();
