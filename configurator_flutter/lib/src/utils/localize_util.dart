fb6d32deec190af92ad58684a1c187898606e828
cgiuliani <camrongiuliani@pm.me>
2024-02-19 14:52:11 -0500

    fix: Breaking changes from upstream

--- configurator_flutter/lib/src/utils/localize_util.dart
+++ configurator_flutter/lib/src/utils/localize_util.dart
@@ -5,10 +5,7 @@ class LocalizeUtil {
   static String localize(Configuration config, String input) {
     return i18n.localize(
       input,
-      i18n.Translations.from(
-        "en_us",
-        config.currentTranslations(input),
-      ),
+      i18n.Translations.byLocale('en_us') + config.currentTranslations(input),
     );
   }
 }
